66cf5c841b6e9d131d7aa1f9e94656dec617c3b6
Wenchao Wang <unlock.liberty@gmail.com>
2015-05-13 17:33:16 -0400

    added configuration file

--- GTAVMod_Speedometer/SpeedoScript.cs
+++ GTAVMod_Speedometer/SpeedoScript.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using GTA;
 using GTA.Math;
 using GTA.Native;
@@ -17,14 +18,12 @@ namespace GTAVMod_Speedometer
         UIText speedText;
         const int PANEL_WIDTH = 66;
         const int PANEL_HEIGT = 24;
+        bool useMph;
 
         public SpeedoScript()
         {
-            // Set up HUD container
-            this.hudContainer = new UIContainer(new Point(UI.WIDTH/2 - PANEL_WIDTH/2, UI.HEIGHT - PANEL_HEIGT), 
-                new Size(PANEL_WIDTH, PANEL_HEIGT), Color.FromArgb(200, 237, 239, 241));
-            this.speedText = new UIText("SPEEDO", new Point(PANEL_WIDTH/2, 0), 0.5f, Color.Black, 4, true);
-            this.hudContainer.Items.Add(speedText);
+            // Read configuration
+            ParseSettings();
 
             this.Tick += OnTick;
         }
@@ -35,8 +34,45 @@ namespace GTAVMod_Speedometer
             {
                 Vehicle vehicle = Game.Player.Character.CurrentVehicle;
                 float speedKph = vehicle.Speed * 3600 / 1000;   // convert from m/s to km/h
-                speedText.Text = speedKph.ToString("0") + " km/h";
+                float speedMph = speedKph * 0.6213711916666667f; // convert km/h to mph
+                if (useMph)
+                    speedText.Text = speedMph.ToString("0") + " mph";
+                else
+                    speedText.Text = speedKph.ToString("0") + " km/h";
+                
                 hudContainer.Draw();
+            }
+        }
+
+        void ParseSettings()
+        {
+            try
+            {
+                ScriptSettings settings = ScriptSettings.Load(@".\scripts\Metric_Speedometer.ini");
+                
+                // Parse Core settings
+                this.useMph = settings.GetValue("Core", "UseMph", true);
+
+                // Parse UI settings
+                VerticalAlignment vAlign = (VerticalAlignment)Enum.Parse(typeof(VerticalAlignment), settings.GetValue("UI", "VertAlign"));
+                HorizontalAlign hAlign = (HorizontalAlign)Enum.Parse(typeof(HorizontalAlign), settings.GetValue("UI", "HorzAlign"));
+                Point posOffset = new Point(settings.GetValue<int>("UI", "OffsetX", 0), settings.GetValue<int>("UI", "OffsetY", 0));
+                Color backcolor = Color.FromArgb(settings.GetValue<int>("UI", "BackcolorA", 200), settings.GetValue<int>("UI", "BackcolorR", 237),
+                    settings.GetValue<int>("UI", "BackcolorG", 239), settings.GetValue<int>("UI", "BackcolorB", 241));
+                Color forecolor = Color.FromArgb(settings.GetValue<int>("UI", "ForecolorA", 255), settings.GetValue<int>("UI", "ForecolorR", 0),
+                    settings.GetValue<int>("UI", "ForecolorG", 0), settings.GetValue<int>("UI", "ForecolorB", 0));
+
+                // Set up HUD container
+                this.hudContainer = new UIContainer(new Point(UI.WIDTH / 2 - PANEL_WIDTH / 2, UI.HEIGHT - PANEL_HEIGT),
+                    new Size(PANEL_WIDTH, PANEL_HEIGT), backcolor);
+                this.speedText = new UIText("SPEEDO", new Point(PANEL_WIDTH / 2, 0), 0.5f, forecolor, 4, true);
+                this.hudContainer.Items.Add(speedText);
+            }
+            catch (Exception exc)
+            {
+                Wait(10000);
+                UI.ShowSubtitle(exc.ToString(), 10000);
+                this.Abort();
             }
         }
     }
---
+++ GTAVMod_Speedometer/readme.txt
@@ -0,0 +1,15 @@
+﻿Simple Metric/Imperial Speedometer
+by libertylocked
+
+A very simple and lite .NET script that shows your vehicle speed in km/h or mph. 
+
+The filename is "Metric_Speedometer.dll", but it also supports MPH.
+Details on how to change the config can be seen in the INI file.
+
+This mod requires:
+Script Hook V 
+Script Hook V Dot Net 
+
+How to install: 
+1. Put "Metric_Speedometer.dll" and "Metric_Speedometer.ini" in "scripts" folder.
+2. Open "Metric_Speedometer.ini" and change the configuration to your preference.
