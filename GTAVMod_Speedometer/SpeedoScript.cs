94882a0891c2ae1c3cfed90a49e179e30e61078a
Wenchao Wang <unlock.liberty@gmail.com>
2015-05-13 02:29:49 -0400

    Added Speedometer script

--- GTAVMod_Speedometer/SpeedoScript.cs
+++ GTAVMod_Speedometer/SpeedoScript.cs
@@ -13,5 +13,31 @@ namespace GTAVMod_Speedometer
 {
     public class SpeedoScript : Script
     {
+        UIContainer hudContainer;
+        UIText speedText;
+        const int PANEL_WIDTH = 66;
+        const int PANEL_HEIGT = 24;
+
+        public SpeedoScript()
+        {
+            // Set up HUD container
+            this.hudContainer = new UIContainer(new Point(UI.WIDTH/2 - PANEL_WIDTH/2, UI.HEIGHT - PANEL_HEIGT), 
+                new Size(PANEL_WIDTH, PANEL_HEIGT), Color.FromArgb(200, 237, 239, 241));
+            this.speedText = new UIText("SPEEDO", new Point(PANEL_WIDTH/2, 0), 0.5f, Color.Black, 4, true);
+            this.hudContainer.Items.Add(speedText);
+
+            this.Tick += OnTick;
+        }
+
+        void OnTick(object sender, EventArgs e)
+        {
+            if (Game.Player.IsAlive && Game.Player.Character.IsInVehicle())
+            {
+                Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+                float speedKph = vehicle.Speed * 3600 / 1000;   // convert from m/s to km/h
+                speedText.Text = speedKph.ToString("0") + " km/h";
+                hudContainer.Draw();
+            }
+        }
     }
 }
