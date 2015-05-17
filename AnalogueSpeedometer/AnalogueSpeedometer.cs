b762b032dcbf0d124a947d4d071bd16b6c513be8
Wenchao Wang <unlock.liberty@gmail.com>
2015-05-17 05:39:28 -0400

    get the analogue speedometer project started

---
+++ AnalogueSpeedometer/AnalogueSpeedometer.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Analogue Metric/Imperial Speedometer
+ * Author: libertylocked
+ * Version: 0.1a
+ */
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using GTA;
+using GTA.Native;
+
+namespace AnalogueSpeedometer
+{
+    public class AnalogueSpeedometer : Script
+    {
+    }
+}
